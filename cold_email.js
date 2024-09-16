a8c8b0470c7391ca12e9d39b9bb0353045f5f87a
cake <ckchcu@gmail.com>
2024-09-16 10:30:19 -0700

    updated email script

--- cold_email.js
+++ cold_email.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const dbFile = 'emails.db';
 const credentials = require('./credentials.json');
-const gmailAcct = credentials.HANNAH;
+const gmailAcct = credentials.JAMES;
 
 // Function to initialize SQLite database
 async function initializeDatabase() {
@@ -115,24 +115,60 @@ async function main() {
       }
     });
     const senderEmail = `${gmailAcct.EMAIL}`; // Update with sender's email
-    const emailList = ['haydenkauk@gmail.com','edmond@thepeakok.com','dispo@taboocannasw.com','wheelercannabis@gmail.com','kavascorp@gmail.com','happyplacecannabis@gmail.com','phamilytreesdispo@gmail.com','info@bdzcannabisok.com','asully1104@gmail.com','officialgreenonion59@gmail.com','incpatel@yahoo.com','405budzsouth@gmail.com','caliyield405@gmail.com','420la2ok@gmail.com','ultraleafok@gmail.com','grnplus420@gmail.com','info@puffydispensary.com','dispensarynm.info@gmail.com','superdankmoore@gmail.com','royalbudzdispensary@gmail.com','nativeleafdispensary@gmail.com','thehustleokc@gmail.com','okcsouth@freedomroad.com','info@kindokc.com','casey@gasdiehler.com','bakersmedical@gmail.com','chickasha@starbuds.us','prestigecannabishome@gmail.com','caliyielddelcity@gmail.com','highway2healthcbd@gmail.com']
+    const emailList = ['uwdok1@gmail.com','harrah4greenleaf@gmail.com','bonnie@brixxcultivation.com','stillwater@shopcannacure.com','info@shopcannacure.com','shakeshackok@gmail.com','beyondthepinesok@gmail.com','getbakdshawnee@gmail.com','greensupremeseminole@gmail.com','bigdaddysgasco@gmail.com','cannabismarketplaceok@gmail.com','420.uptown.wellness@gmail.com','mainstreetmeds@gmail.com','reupdispensary@gmail.com','travis@kindloveok.com','mwedman@grogenexusa.com','sales@cake-cannabis.com','flavorchasersdispensary@gmail.com','thenativenugs@gmail.com','info@skyleafok.com','herbalmedsdispensaryoneastpine@gmail.com','orders@greencountrybud.com','okieogcannabis@gmail.com','cannalandok@gmail.com','luckysevendispo@gmail.com','topshelfstocktulsa@gmail.com','tulsagreentherapy@gmail.com','greencraft918@gmail.com','ttownmedicalrx@gmail.com','alfred@endo-holdings.com']
     const mailOptions = {
       from: `"${gmailAcct.NAME}" <${gmailAcct.EMAIL}>`,
-      // subject: `Seamless Transactions, Increased Revenue: Try Our Credit Card Processing Solutions`,
-      // subject: `Tap Into Convenience: Enable Apple Pay for Your Dispensary`,
       //subject: `Cashless ATMs: The Smart Choice for Your Dispensary`,
-      subject: `Transform Your Dispensary with Tap to Pay & Mobile Payments`,
+      //subject: `Transform Your Dispensary with Tap to Pay & Mobile Payments`,
+      subject: `Transform Your Dispensary Payments – Accept Major Credit Cards & Mobile!`,
+      //subject: `Upgrade Your Payment System: Debit & Credit Solutions for Dispensaries`,
+      //subject: `Streamline Your Payment Processing with the Best Rates`,
       text: 
-      //Cashless ATM if they are a dispo #1
+      //CC #1
+      // `Hi,\n\n` +
+      // `Are you currently looking for a credit card solution that could boost your dispensary\’s revenue?\n\n` +
+      // `Our system offers Tap to Pay, Apple Pay, and Google Pay, making transactions quick and convenient for your customers. Dispensaries that accept credit cards typically see a 30% to 70% increase in sales. Plus, it\’s easy to set up and integrates seamlessly with your existing system.\n\n` +
+      // `If this sounds like something you\’re interested in, let\’s set up a quick call to discuss the details. I\’d love to help you get started.\n\n` +
+      // 'Best,\n' +
+      // `${gmailAcct.NAME}\n` +
+      // 'Business Development Representative\n' +
+      // 'Spectrum Payment Solutions\n' +
+      // `Email: ${gmailAcct.EMAIL}`
+
+      //CC #2
       `Hi,\n\n` +
-      `Are you currently looking for a credit card solution that could boost your dispensary\’s revenue?\n\n` +
-      `Our system offers Tap to Pay, Apple Pay, and Google Pay, making transactions quick and convenient for your customers. Dispensaries that accept credit cards typically see a 30% to 70% increase in sales. Plus, it\’s easy to set up and integrates seamlessly with your existing system.\n\n` +
-      `If this sounds like something you\’re interested in, let\’s set up a quick call to discuss the details. I\’d love to help you get started.\n\n` +
+      `Are you currently looking for a credit card solution that can boost your dispensary’s revenue?\n\n` +
+      `Our system allows you to accept all major credit cards, as well as Tap to Pay, Apple Pay, and Google Pay, making transactions quick and convenient for your customers. Dispensaries that accept credit cards and mobile payments typically see a 30% to 70% increase in sales.\n\n` +
+      `If this sounds like something you\’re interested in, I\’d be happy to set up a quick call to discuss the details and help you get started.\n\n` +
       'Best,\n' +
       `${gmailAcct.NAME}\n` +
       'Business Development Representative\n' +
       'Spectrum Payment Solutions\n' +
       `Email: ${gmailAcct.EMAIL}`
+
+      //Both #1
+      // `Hi,\n\n` +
+      // `Are you seeking a way to enhance your dispensary's payment options and boost your sales? We offer separate credit and debit card solutions designed to do just that.\n\n` +
+      // `- For debit, our Cashless ATM solution starts with a $2.50 surcharge and gives you the option to earn a rebate on every transaction.\n` +
+      // `- Our credit card solution has a starting rate of 4.95% + $3 per transaction and accepts all major credit cards, including Tap to Pay & Mobile Payments. We also offer the option to add a fee to the customer’s bill to offset the rate to the merchant, helping to keep your costs down.\n` +
+      // `- Both solutions are easy to set up, work for in-store and delivery services, and come with no contracts or maintenance fees.\n\n` +
+      // `I’d love to set up a quick call to discuss how our payment solutions can benefit your dispensary. When would be a good time for you?\n\n` +
+      // `Looking forward to helping you elevate your business.\n\n` +
+      // 'Best,\n' +
+      // `${gmailAcct.NAME}\n` +
+      // 'Business Development Representative\n' +
+      // 'Spectrum Payment Solutions\n' +
+      // `Email: ${gmailAcct.EMAIL}`
+
+      //Both #2
+      // `Hi,\n\n` +
+      // `Are you interested in upgrading your payment options? We offer a cashless ATM solution with 4G compatibility—great for delivery services—and you get rebates on every transaction. Plus, our credit card solution accepts mobile payments, making it super convenient for your customers.\n\n` +
+      // `Setup is easy, and we provide free terminals to get you going. Let\’s connect and see how we can help you improve your payment processing!\n\n` +
+      // 'Best,\n' +
+      // `${gmailAcct.NAME}\n` +
+      // 'Business Development Representative\n' +
+      // 'Spectrum Payment Solutions\n' +
+      // `Email: ${gmailAcct.EMAIL}`
 
       //Cashless ATM #1 if they are a dispo #2
       // 'Hi,\n\n' +
@@ -156,6 +192,17 @@ async function main() {
       // 'Business Development Representative\n' +
       // 'Spectrum Payment Solutions\n' +
       // `Email: ${gmailAcct.EMAIL}`
+
+      //Vape Stores
+      // `Hi,\n\n` +
+      // `Are you having issues with your current payment processor, like restrictions on products such as D8, Glass, or Kratom, volume limits being capped, or money being held in reserves? We can help. Since 2008, we've been the first smoke processor, partnering with the biggest brands and wholesalers.\n\n` +
+      // `We offer rates as low as 1.50% for stores and 2.75% for brands and wholesalers. Let us improve your revenue and lower your costs without any downtime or additional expenses. Interested in learning more? Let\’s connect!\n\n` +
+      // 'Best,\n' +
+      // `${gmailAcct.NAME}\n` +
+      // 'Business Development Representative\n' +
+      // 'Spectrum Payment Solutions\n' +
+      // `Email: ${gmailAcct.EMAIL}`    
+      
       
     };
     await sendEmails(db, transporter, senderEmail, emailList, mailOptions);
